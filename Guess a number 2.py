a3b82768be5ce8fec7e8bd33dddf2f9b9350fd71
jayeshmaniar <51348677+jayeshmaniar@users.noreply.github.com>
2019-06-08 07:14:05 -0500

    Add files via upload

---
+++ Guess a number 2.py
@@ -0,0 +1,23 @@
+# Random Number 
+# Jayesh Maniar
+# Seed random number
+import random
+# random number rn between 1 and 10
+rn = random.randint(1,10)
+
+gn = 0
+
+while rn != gn: 
+    # input guess gn
+    gn = int(input('choose a number: '))
+    # compare guess to random number
+    if gn == rn:
+        print('correct')
+        break
+    else:
+        if gn > rn:
+            print('Too high, guess again')
+        else:
+            print('Too low, guess again')
+        
+        
